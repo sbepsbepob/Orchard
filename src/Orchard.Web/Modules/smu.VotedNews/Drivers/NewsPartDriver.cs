ccf1ea6196837c7bd5c0c63214d51524e878f38f
Slobodan Mumovic <slobodan@ever-near.com>
2015-11-04 17:14:53 +0100

    Added combined shapes for the NewsPart

--- src/Orchard.Web/Modules/smu.VotedNews/Drivers/NewsPartDriver.cs
+++ src/Orchard.Web/Modules/smu.VotedNews/Drivers/NewsPartDriver.cs
@@ -3,11 +3,25 @@ using System.Linq;
 using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
 using Smu.VotedNews.Models;
+using Orchard.Comments.Services;
+using Orchard.Comments.Models;
 
 namespace Smu.VotedNews.Drivers
 {
     public class NewsPartDriver : ContentPartDriver<NewsPart>
     {
+
+        private readonly ICommentService _commentService;
+        private readonly IContentManager _contentManager;
+
+        public NewsPartDriver(
+            ICommentService commentService,
+            IContentManager contentManager)
+        {
+            _commentService = commentService;
+            _contentManager = contentManager;
+        }
+
 
         protected override string Prefix
         {
@@ -28,11 +42,63 @@ namespace Smu.VotedNews.Drivers
 
         protected override DriverResult Display(NewsPart news, string displayType, dynamic shapeHelper)
         {
-            return ContentShape("Parts_News", () => shapeHelper.Parts_News(
-                IsFeatured: news.IsFetured,
-                IsSticky: news.IsSticky,
-                Weight: news.Weight
-            ));
+//            var newsCommentAgree = _contentManager.Query<CommentsPart, CommentsPartRecord>().
+            return Combined(
+                
+                // Shape 1: Parts_News
+                ContentShape("Parts_News", () => shapeHelper.Parts_News(
+                    IsFeatured: news.IsFetured,
+                    IsSticky: news.IsSticky,
+                    Weight: news.Weight
+                )),
+
+                // Shape 2: Parts_News_VoteButtons
+                ContentShape("Parts_News_VoteButtons", () => shapeHelper.Parts_News_VoteButtons(
+                    NewsId: news.Id
+                )),
+
+                // Shape 3: Parts_News_Comments
+                ContentShape("Parts_News_Comments", () => shapeHelper.Parts_News_Comments(
+                )),
+
+                ContentShape("Parts_TopAgreeComment",
+                    () => {
+                        // create a hierarchy of shapes
+                        var comments = _commentService
+                            .GetCommentsForCommentedContent(news.ContentItem.Id)
+                            .Where(x => x.Status == CommentStatus.Approved && x.ContentItemRecord.EnumerationField.Value == "Agree")
+                            .OrderBy(x => x.Position)
+                            .List()
+                            .ToList();
+
+                        foreach (var item in comments)
+                        {
+                            var shape = shapeHelper.Parts_Comment(ContentPart: item, ContentItem: item.ContentItem);
+                            allShapes.Add(item.Id, shape);
+                        }
+
+                        foreach (var item in comments)
+                        {
+                            var shape = allShapes[item.Id];
+                            if (item.RepliedOn.HasValue)
+                            {
+                                allShapes[item.RepliedOn.Value].Add(shape);
+                            }
+                            else
+                            {
+                                firstLevelShapes.Add(shape);
+                            }
+                        }
+
+                        var list = shapeHelper.List(Items: firstLevelShapes);
+
+                        return shapeHelper.Parts_ListOfComments(
+                            List: list,
+                            CommentCount: part.CommentsCount);
+                    })
+
+            );
+
         }
 
 
